ae203b9acf9b0a5696a3b4fc9ca8aaa1b4fac128
ViniMPDS11 <vinimpdevs@gmail.com>
2025-05-15 17:25:38 -0300

    aula 2

---
+++ scripts/create-point.js
@@ -0,0 +1,53 @@
+const ufSelect = document.querySelector('select[name=state]');
+const citySelect = document.querySelector('select[name=city]');
+
+ufSelect.addEventListener("change", () => {
+    const selectedOption = ufSelect.options[ufSelect.selectedIndex];
+    const dataId = selectedOption.getAttribute('data-id');
+
+    populateCities(dataId);
+    if (dataId === '0') {
+        citySelect.disabled = true;
+    } else {
+        citySelect.disabled = false;
+    }
+})
+
+populateUFs();
+
+async function populateUFs() {
+    try {
+        const response = await fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados");
+        const data = await response.json();
+
+        data.forEach(item => {
+            const option = document.createElement('option');
+            option.setAttribute('data-id', item.id);
+            option.setAttribute('value', item.sigla);
+            option.innerText = `${item.nome}`;
+
+            ufSelect.appendChild(option); 
+        });
+    } catch {
+
+    }
+}
+
+async function populateCities(id) {
+    try {
+        const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios`);
+        const data = await response.json();
+        
+        citySelect.innerHTML = '<option value="0">Selecione a Cidade</option>';
+        data.forEach(item => {
+            const option = document.createElement('option');
+            option.setAttribute('data-id', item.id);
+            option.setAttribute('value', item.nome);
+            option.innerText = `${item.nome}`;
+
+            citySelect.appendChild(option);
+        })
+    } catch {
+
+    }
+}
